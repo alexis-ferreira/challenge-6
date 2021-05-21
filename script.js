ea7507df9d37aea1442fa763096cd7d0c4b66f6c
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-05-21 15:51:32 +0200

    Update

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ let btnAccordion = document.getElementsByClassName('accordion');
 let contentAccordion = document.getElementsByClassName('content');
 // console.log(contentAccordion);
 let arrows = document.getElementsByClassName('arrow')
-console.log(arrows);
+// console.log(arrows);
 let box = document.getElementById('box');
-console.log(box);
+// console.log(box);
 
 for (let i = 0; i < btnAccordion.length; i++){
 
@@ -18,13 +18,19 @@ for (let i = 0; i < btnAccordion.length; i++){
         // console.log(content);
         if (content.style.maxHeight){
             content.style.maxHeight=null;
-            console.log(content);
+            // console.log(content);
             arrows[i].style.transform='rotate(0deg)';
+            box.style.left='-93px';
+            btnAccordion[i].style.fontWeight="400";
+            btnAccordion[i].style.color="";
         }
         else{
             content.style.maxHeight = content.scrollHeight + "px";
-            console.log(content);
+            // console.log(content);
             arrows[i].style.transform='rotate(180deg)';
+            box.style.left='-135px';
+            btnAccordion[i].style.fontWeight="700";
+            btnAccordion[i].style.color="hsl(238, 29%, 16%)";
         }
     });
 };
