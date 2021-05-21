d8ebb679e2ddde0fdaaebb7cfd6b56c440e8ec78
Alexis Ferreira <alexis.ferreira71@gmail.com>
2021-05-21 21:38:48 +0200

    Update

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let arrows = document.getElementsByClassName('arrow')
 // console.log(arrows);
 let box = document.getElementById('box');
 // console.log(box);
+// console.log(screen.width);
 
 for (let i = 0; i < btnAccordion.length; i++){
 
@@ -20,17 +21,25 @@ for (let i = 0; i < btnAccordion.length; i++){
             content.style.maxHeight=null;
             // console.log(content);
             arrows[i].style.transform='rotate(0deg)';
-            box.style.left='38px';
             btnAccordion[i].style.fontWeight="400";
             btnAccordion[i].style.color="";
+            if (screen.width < 421){
+                box.style.left='38px';
+            } else{
+                box.style.left='-93px';
+            }
         }
         else{
             content.style.maxHeight = content.scrollHeight + "px";
             // console.log(content);
             arrows[i].style.transform='rotate(180deg)';
-            box.style.left='18px';
             btnAccordion[i].style.fontWeight="700";
             btnAccordion[i].style.color="hsl(238, 29%, 16%)";
+            if (screen.width < 421){
+                box.style.left='15px';
+            } else{
+                box.style.left='-125px';
+            }
         }
     });
 };
